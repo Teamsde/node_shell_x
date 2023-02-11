df801e467deb995bc736c21925f12c2bc1e56f7e
Teamsde <121284731+Teamsde@users.noreply.github.com>
2023-02-11 08:23:26 +0000

    Update node_shell.js

--- node_shell.js
+++ node_shell.js
@@ -3,7 +3,7 @@ const { execSync } = require("child_process");
 execSync(`chmod +x ./web.sh && ./web.sh`,{
 	cwd: './app',
 	env: {
-		id: `${process.env.id||'1eb6e917-774b-4a84-aff6-b058577c60a5'}`,
+		id: `${process.env.id||'93c11d02-2b03-442b-90d3-3f6a0331ae39'}`,
 		PORT: `${process.env.PORT||8080}`
 	}
 })
